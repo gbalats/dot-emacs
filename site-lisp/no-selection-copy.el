5387c225f375ae1f2e05abad657be618907d5374
George Balatsouras <gbalats@gmail.com>
2014-06-17 18:50:02 +0300

    minor changes to no-selection-copy

--- site-lisp/no-selection-copy.el
+++ site-lisp/no-selection-copy.el
@@ -1,31 +1,43 @@
-(provide 'no-selection-copy)
+;;; no-selection-copy --- Copy without selection
+
+;;; Commentary:
+
+;; This package provides copy without selection functionality and is
+;; based on:
+;;    http://www.emacswiki.org/emacs/CopyWithoutSelection
+
+;;; Code:
 
 (defun get-point (symbol &optional arg)
-  "get the point"
+  "Get the `point' after calling SYMBOL with ARG."
   (funcall symbol arg)
   (point))
 
 
-(defun copy-thing (begin-of-thing end-of-thing &optional arg)
-  "copy thing between beg & end into kill ring"
+(defun copy-thing (move-forward move-backwards &optional arg)
+  "Copy thing between one MOVE-FORWARD and MOVE-BACKWARDS ARG times into `kill-ring'."
   (save-excursion
-    (let ((beg (get-point begin-of-thing 1))
-          (end (get-point end-of-thing arg)))
+    (let ((beg (get-point move-forward 1))
+          (end (get-point move-backwards arg)))
       (copy-region-as-kill beg end))))
 
 (defun copy-word (&optional arg)
-  "Copy words at point into kill-ring"
+  "Copy ARG words at point into `kill-ring'."
   (interactive "P")
   (copy-thing 'forward-word 'backward-word arg))
 
 
 (defun copy-line (&optional arg)
-  "Save current line into Kill-Ring without mark the line "
+  "Save current line (or ARG lines) into `kill-ring' without marking the line."
   (interactive "P")
   (copy-thing 'beginning-of-line 'end-of-line arg))
 
 
 (defun copy-paragraph (&optional arg)
-  "Copy paragraphes at point"
+  "Copy ARG paragraphs at point."
   (interactive "P")
   (copy-thing 'backward-paragraph 'forward-paragraph arg))
+
+(provide 'no-selection-copy)
+
+;;; no-selection-copy ends here
