a3f7e5f1a30dd51cae9fdf0c2f6f167dbc5536b3
George Balatsouras <gbalats@gmail.com>
2014-06-17 18:51:53 +0300

    minor changes to key-bindings

--- site-lisp/key-bindings.el
+++ site-lisp/key-bindings.el
@@ -1,3 +1,4 @@
+;;; key-bindings --- My personal keybindings
 ;;; Commentary:
 
 ;; Put this file into your load-path and the following into your ~/.emacs:
@@ -5,10 +6,12 @@
 
 ;;; Code:
 
-(provide 'key-bindings)
-
 ;; set the keybinding so that you can use f4 for goto line
 (global-set-key [f4] 'goto-line)
 
 ;;; Since `M-<SPC>' is used by gnome, use `C-c <SPC>' instead
 (global-set-key (kbd "C-c <SPC>") 'just-one-space)
+
+(provide 'key-bindings)
+
+;;; key-bindings ends here
