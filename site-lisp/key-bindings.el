c3501989ffcfca6487abefd75c15c344f7099590
George Balatsouras <gbalats@gmail.com>
2014-06-17 19:38:38 +0300

    Adding comment to key-bindings

--- site-lisp/key-bindings.el
+++ site-lisp/key-bindings.el
@@ -1,6 +1,11 @@
 ;;; key-bindings --- My personal keybindings
+
 ;;; Commentary:
 
+;; This package has shrinked considerably since the adoption of
+;; `use-package', and now only contains some generic global
+;; keybindings.
+;;
 ;; Put this file into your load-path and the following into your ~/.emacs:
 ;;   (require 'key-bindings)
 
