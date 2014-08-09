982e57e5fdf612e23bb460fdd03f9311872b2403
George Balatsouras <gbalats@gmail.com>
2014-08-09 14:42:40 +0300

    Minor change

--- init.el
+++ init.el
@@ -299,7 +299,6 @@
   :ensure t
   :config
   ;; Remove Yasnippet's default tab key binding
-  (define-key yas-minor-mode-map (kbd "<tab>") nil)
   (define-key yas-minor-mode-map (kbd "TAB") nil)
   ;; Set Yasnippet's key binding to shift+tab
   (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
