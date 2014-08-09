dddbb1b93c6599336a9a511df7ca4525aa176e93
George Balatsouras <gbalats@gmail.com>
2014-08-09 14:15:16 +0300

    Changing YASnippet binding to shift+TAB

--- init.el
+++ init.el
@@ -297,6 +297,12 @@
 ;; YASnippet Programming Templates
 (use-package yasnippet
   :ensure t
+  :config
+  ;; Remove Yasnippet's default tab key binding
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  ;; Set Yasnippet's key binding to shift+tab
+  (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
   :init
   (yas-global-mode 1))
 
