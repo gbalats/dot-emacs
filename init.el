0ab70f091aad7bdbe65129d474282e896c090405
George Balatsouras <gbalats@gmail.com>
2016-03-19 00:28:14 +0200

    Add some key bindings to ggtags

--- init.el
+++ init.el
@@ -256,6 +256,12 @@
   :ensure t
   :commands ggtags-mode
   :diminish ggtags-mode
+  :bind (("M-," . pop-tag-mark)
+         ("C-c t s" . ggtags-find-other-symbol)
+         ("C-c t h" . ggtags-view-tag-history)
+         ("C-c t r" . ggtags-find-reference)
+         ("C-c t f" . ggtags-find-file)
+         ("C-c t c" . ggtags-create-tags))
   :init
   (add-hook 'c-mode-common-hook
             #'(lambda ()
